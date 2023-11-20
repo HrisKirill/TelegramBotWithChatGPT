6c13264564511d1ddd00c47d9aaeadc52d4d9d3a
HrisKirill <hristoforow.kirill@gmail.com>
2023-11-20 15:33:54 +0200

    add method askBot in UserController

--- src/main/java/com/khrystoforov/telegrambotwithchatgpt/controller/UserController.java
+++ src/main/java/com/khrystoforov/telegrambotwithchatgpt/controller/UserController.java
@@ -1,12 +1,11 @@
 package com.khrystoforov.telegrambotwithchatgpt.controller;
 
-import com.khrystoforov.telegrambotwithchatgpt.mapper.RequestHistoryMapper;
+import com.khrystoforov.telegrambotwithchatgpt.TelegramBot;
 import com.khrystoforov.telegrambotwithchatgpt.payload.dto.RequestHistoryDTO;
+import com.khrystoforov.telegrambotwithchatgpt.payload.response.MessageResponse;
 import com.khrystoforov.telegrambotwithchatgpt.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,9 +14,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final TelegramBot telegramBotService;
 
     @GetMapping("/show-history")
     public List<RequestHistoryDTO> showHistory() {
         return userService.getUserHistory();
+    }
+
+    @PostMapping("/ask-bot")
+    public MessageResponse askBot(@RequestParam(name = "question") String question) {
+        return telegramBotService.askGpt(userService.getCurrentUser().getChatId(), question);
     }
 }
