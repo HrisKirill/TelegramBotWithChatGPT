c2260e6aec25acaddab9a80476cc212a5f304854
HrisKirill <hristoforow.kirill@gmail.com>
2023-11-20 15:33:15 +0200

    add length to question and answer

--- src/main/java/com/khrystoforov/telegrambotwithchatgpt/model/RequestHistory.java
+++ src/main/java/com/khrystoforov/telegrambotwithchatgpt/model/RequestHistory.java
@@ -17,7 +17,9 @@ public class RequestHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length = 1000)
     private String question;
+    @Column(length = 5000)
     private String answer;
     private LocalDateTime time;
 }
